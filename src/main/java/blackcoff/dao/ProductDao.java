9666713b28d5b45cf9831878566a0d180f699556
TEJAS NAIK <TEJAS NAIK@LAPTOP-N33912D5>
2023-02-11 17:43:12 +0530

    yaah

---
+++ src/main/java/blackcoff/dao/ProductDao.java
@@ -0,0 +1,48 @@
+package blackcoff.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+
+import blackcoff.model.Product;
+
+@Component
+public class ProductDao
+{
+    @Autowired
+	private HibernateTemplate hibernateTemplate;
+    
+    
+    //for creating product
+    @Transactional
+    public void createProduct(Product product)
+    {
+    	this.hibernateTemplate.saveOrUpdate(product);
+    }
+    
+    //for getting products
+    public List<Product> getProducts()
+    {
+    	List <Product> products=this.hibernateTemplate.loadAll(Product.class);
+    	return products;
+    }
+    //delete single product
+    @Transactional
+    public void deleteProduct(int pid) 
+    {
+    	Product p=this.hibernateTemplate.load(Product.class, pid);
+    	this.hibernateTemplate.delete(p);
+    }
+    
+    public Product getProduct(int pid) 
+    {
+    	return this.hibernateTemplate.get(Product.class, pid);
+    }
+    
+    
+}
